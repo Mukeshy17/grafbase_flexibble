7da48733208ed76bbfdbbbb24368be2ae8bff482
Anand <anandkuyadav32@gmail.com>
2023-10-31 01:28:52 +0530

    second commit

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,26 +1,26 @@
 import { g, auth, config } from '@grafbase/sdk'
 
-const User = g.model('User',{
-    name:g.string.length({min:2,max:20}),
-    email:g.string().unique(),
-    avatarUrl:g.url(),
-    description:g.string().optional(),
-    github:g.url().optional(),
-    linkedInUrl:g.url().optional(),
-    project:g.relation(()=>Project).list().optional(),
+const User = g.model('User', {
+  name: g.string().length({ min: 2, max: 100 }),
+  email: g.string().unique(),
+  avatarUrl: g.url(),
+  description: g.string().length({ min: 2, max: 1000 }).optional(),
+  githubUrl: g.url().optional(),
+  linkedinUrl: g.url().optional(), 
+  projects: g.relation(() => Project).list().optional(),
 })
 
-const Project = g.model('Project',{
-    title:g.string().length({min:3}),
-    description:g.string().optional(),
-    image:g.url(),
-    liveSiteUrl:g.url(),
-    githubUrl:g.url(),
-    category:g.url(),
-    createdBy:g.relation(()=>User),
-}
-
+const Project = g.model('Project', {
+  title: g.string().length({ min: 3 }),
+  description: g.string(), 
+  image: g.url(),
+  liveSiteUrl: g.url(), 
+  githubUrl: g.url(), 
+  category: g.string().search(),
+  createdBy: g.relation(() => User),
+})
 
 export default config({
-    schema:g
+  schema: g
+
 })
